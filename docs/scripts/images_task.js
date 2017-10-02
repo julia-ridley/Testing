9d0fa9016bd22afde83142018ee1d80152adffc6
julia-ridley <32227285+julia-ridley@users.noreply.github.com>
2017-10-01 23:12:22 -0400

    Add files via upload

---
+++ docs/scripts/images_task.js
@@ -0,0 +1,33 @@
+var numClicks = null;
+var elementHistory = [];
+
+$(document).ready(function() {
+	$("#theimage").click(function(e) {
+		numClicks++;
+		$("#numpeoplefield").val(numClicks);
+		$("#numpeople").text(numClicks);
+		var newDomElement = $("<div class='marker'></div>");
+		newDomElement.css("left", (e.pageX-3)+"px");
+		newDomElement.css("top", (e.pageY-3)+"px");
+		$("body").append(newDomElement);
+		elementHistory.push(newDomElement);
+		var currentHistory = $("#history");
+		currentHistory.append("|"+e.PageX+","+e.PageY+"|");
+		$("#history").val(currentHistory);
+	});
+	$("#undobutton").click(function() {
+		numClicks--;
+		$("#numpeoplefield").val(numClicks);
+		$("#numpeople").text(numClicks);
+		var elementToRemove = elementHistory.pop();
+		elementToRemove.remove();
+	});
+	$("#increasesizebutton").click(function() {
+		var imgheight=$("#theimage").height;
+		if (imgheight<1000) {
+			imgheight+=200;
+			$("#theimage").height.(imgheight);
+		}
+	});
+
+})
