9a8c08cc9f397941e819cc6dfcc5cc0c53cbc0f4
julia-ridley <32227285+julia-ridley@users.noreply.github.com>
2017-10-03 15:45:17 -0400

    Add files via upload

--- docs/scripts/images_task.js
+++ docs/scripts/images_task.js
@@ -1,31 +1,39 @@
 var numClicks1 = null;
 var elementHistory1 = [];
+var startTime1 = null;
 var numClicks2 = null;
 var elementHistory2 = [];
+var startTime2 = null;
 var numClicks3 = null;
 var elementHistory3 = [];
+var startTime3 = null;
 var numClicks4 = null;
 var elementHistory4 = [];
+var startTime4 = null;
 var numClicks5 = null;
 var elementHistory5 = [];
+var startTime5 = null;
 var numClicks6 = null;
 var elementHistory6 = [];
+var startTime6 = null;
 
 $(document).ready(function() {
 	//image 1 functions________________________________________
+	$("#people_1").one("click", function (){
+		startTime1 = performance.now();
+	});
 	$("#people_1").click(function(e) {
 		numClicks1++;
 		$("#numpeoplefield1").val(numClicks1);
-		console.log(numClicks1);
 		$("#peoplecount1").text(numClicks1);
 		var newDomElement = $("<div class='marker'></div>");
 		newDomElement.css("left", (e.pageX-3)+"px");
 		newDomElement.css("top", (e.pageY-3)+"px");
 		$("body").append(newDomElement);
 		elementHistory1.push(newDomElement);
-		var currentHistory = $("#history1");
-		currentHistory.append("|"+e.PageX+","+e.PageY+"|");
-		$("#history1").val(currentHistory);
+		var endTime1 = performance.now();
+		var timeTaken1 = endTime1-startTime1;
+		$("#timetaken1").val(timeTaken1);
 	});
 	$("#undobutton1").click(function() {
 		numClicks1--;
@@ -34,35 +42,23 @@ $(document).ready(function() {
 		var elementToRemove = elementHistory1.pop();
 		elementToRemove.remove();
 	});
-	$("#increasesizebutton1").click(function() {
-		var imgheight = $("#people_1").height;
-		if (imgheight<1000) {
-			imgheight+=200;
-			$("#people_1").height(imgheight);
-		}
-	});
-	$("#decreasesizebutton1").click(function() {
-		var imgheight = $("#people_1").height;
-		if (imgheight>400) {
-			imgheight-=200;
-			$("#people_1").height(imgheight);
-		}
-	});
 
 	//image 2 functions________________________________________
+	$("#people_2").one("click", function (){
+		startTime2 = performance.now();
+	});
 	$("#people_2").click(function(e) {
 		numClicks2++;
 		$("#numpeoplefield2").val(numClicks2);
-		console.log(numClicks2);
 		$("#peoplecount2").text(numClicks2);
 		var newDomElement = $("<div class='marker'></div>");
 		newDomElement.css("left", (e.pageX-3)+"px");
 		newDomElement.css("top", (e.pageY-3)+"px");
 		$("body").append(newDomElement);
 		elementHistory2.push(newDomElement);
-		var currentHistory = $("#history2");
-		currentHistory.append("|"+e.PageX+","+e.PageY+"|");
-		$("#history2").val(currentHistory);
+		var endTime2 = performance.now();
+		var timeTaken2 = endTime2-startTime2;
+		$("#timetaken2").val(timeTaken2);
 	});
 	$("#undobutton2").click(function() {
 		numClicks2--;
@@ -71,35 +67,23 @@ $(document).ready(function() {
 		var elementToRemove = elementHistory2.pop();
 		elementToRemove.remove();
 	});
-	$("#increasesizebutton2").click(function() {
-		var imgheight = $("#people_2").height;
-		if (imgheight<1000) {
-			imgheight+=200;
-			$("#people_2").height(imgheight);
-		}
-	});
-	$("#decreasesizebutton2").click(function() {
-		var imgheight = $("#people_2").height;
-		if (imgheight>400) {
-			imgheight-=200;
-			$("#people_2").height(imgheight);
-		}
-	});
 
 	//image 3 functions________________________________________
+	$("#people_3").one("click", function (){
+		startTime3 = performance.now();
+	});
 	$("#people_3").click(function(e) {
 		numClicks3++;
 		$("#numpeoplefield3").val(numClicks3);
-		console.log(numClicks3);
 		$("#peoplecount3").text(numClicks3);
 		var newDomElement = $("<div class='marker'></div>");
 		newDomElement.css("left", (e.pageX-3)+"px");
 		newDomElement.css("top", (e.pageY-3)+"px");
 		$("body").append(newDomElement);
 		elementHistory3.push(newDomElement);
-		var currentHistory = $("#history3");
-		currentHistory.append("|"+e.PageX+","+e.PageY+"|");
-		$("#history3").val(currentHistory);
+		var endTime3 = performance.now();
+		var timeTaken3 = endTime3-startTime3;
+		$("#timetaken3").val(timeTaken3);
 	});
 	$("#undobutton3").click(function() {
 		numClicks3--;
@@ -108,35 +92,23 @@ $(document).ready(function() {
 		var elementToRemove = elementHistory3.pop();
 		elementToRemove.remove();
 	});
-	$("#increasesizebutton3").click(function() {
-		var imgheight = $("#people_3").height;
-		if (imgheight<1000) {
-			imgheight+=200;
-			$("#people_3").height(imgheight);
-		}
-	});
-	$("#decreasesizebutton3").click(function() {
-		var imgheight = $("#people_3").height;
-		if (imgheight>400) {
-			imgheight-=200;
-			$("#people_3").height(imgheight);
-		}
-	});
 
 	//image 4 functions________________________________________
+	$("#people_4").one("click", function (){
+		startTime4 = performance.now();
+	});
 	$("#people_4").click(function(e) {
 		numClicks4++;
 		$("#numpeoplefield4").val(numClicks4);
-		console.log(numClicks4);
 		$("#peoplecount4").text(numClicks4);
 		var newDomElement = $("<div class='marker'></div>");
 		newDomElement.css("left", (e.pageX-3)+"px");
 		newDomElement.css("top", (e.pageY-3)+"px");
 		$("body").append(newDomElement);
 		elementHistory4.push(newDomElement);
-		var currentHistory = $("#history4");
-		currentHistory.append("|"+e.PageX+","+e.PageY+"|");
-		$("#history4").val(currentHistory);
+		var endTime4 = performance.now();
+		var timeTaken4 = endTime4-startTime4;
+		$("#timetaken4").val(timeTaken4);
 	});
 	$("#undobutton4").click(function() {
 		numClicks4--;
@@ -145,35 +117,23 @@ $(document).ready(function() {
 		var elementToRemove = elementHistory4.pop();
 		elementToRemove.remove();
 	});
-	$("#increasesizebutton4").click(function() {
-		var imgheight = $("#people_4").height;
-		if (imgheight<1000) {
-			imgheight+=200;
-			$("#people_4").height(imgheight);
-		}
-	});
-	$("#decreasesizebutton4").click(function() {
-		var imgheight = $("#people_4").height;
-		if (imgheight>400) {
-			imgheight-=200;
-			$("#people_4").height(imgheight);
-		}
-	});
 
 	//image 5 functions________________________________________
+	$("#people_5").one("click", function (){
+		startTime5 = performance.now();
+	});
 	$("#people_5").click(function(e) {
 		numClicks5++;
 		$("#numpeoplefield5").val(numClicks5);
-		console.log(numClicks5);
 		$("#peoplecount5").text(numClicks5);
 		var newDomElement = $("<div class='marker'></div>");
 		newDomElement.css("left", (e.pageX-3)+"px");
 		newDomElement.css("top", (e.pageY-3)+"px");
 		$("body").append(newDomElement);
 		elementHistory5.push(newDomElement);
-		var currentHistory = $("#history5");
-		currentHistory.append("|"+e.PageX+","+e.PageY+"|");
-		$("#history5").val(currentHistory);
+		var endTime5 = performance.now();
+		var timeTaken5 = endTime5-startTime5;
+		$("#timetaken5").val(timeTaken5);
 	});
 	$("#undobutton5").click(function() {
 		numClicks5--;
@@ -182,35 +142,23 @@ $(document).ready(function() {
 		var elementToRemove = elementHistory5.pop();
 		elementToRemove.remove();
 	});
-	$("#increasesizebutton5").click(function() {
-		var imgheight = $("#people_5").height;
-		if (imgheight<1000) {
-			imgheight+=200;
-			$("#people_5").height(imgheight);
-		}
-	});
-	$("#decreasesizebutton5").click(function() {
-		var imgheight = $("#people_5").height;
-		if (imgheight>400) {
-			imgheight-=200;
-			$("#people_5").height(imgheight);
-		}
-	});
 
 	//image 6 functions________________________________________
+	$("#people_6").one("click", function (){
+		startTime6 = performance.now();
+	});
 	$("#people_6").click(function(e) {
 		numClicks6++;
 		$("#numpeoplefield6").val(numClicks6);
-		console.log(numClicks6);
 		$("#peoplecount6").text(numClicks6);
 		var newDomElement = $("<div class='marker'></div>");
 		newDomElement.css("left", (e.pageX-3)+"px");
 		newDomElement.css("top", (e.pageY-3)+"px");
 		$("body").append(newDomElement);
 		elementHistory6.push(newDomElement);
-		var currentHistory = $("#history6");
-		currentHistory.append("|"+e.PageX+","+e.PageY+"|");
-		$("#history6").val(currentHistory);
+		var endTime6 = performance.now();
+		var timeTaken6 = endTime6-startTime6;
+		$("#timetaken6").val(timeTaken6);
 	});
 	$("#undobutton6").click(function() {
 		numClicks6--;
@@ -219,18 +167,34 @@ $(document).ready(function() {
 		var elementToRemove = elementHistory6.pop();
 		elementToRemove.remove();
 	});
-	$("#increasesizebutton6").click(function() {
-		var imgheight = $("#people_6").height;
-		if (imgheight<1000) {
-			imgheight+=200;
-			$("#people_6").height(imgheight);
-		}
-	});
-	$("#decreasesizebutton6").click(function() {
-		var imgheight = $("#people_6").height;
-		if (imgheight>400) {
-			imgheight-=200;
-			$("#people_6").height(imgheight);
-		}
+
+	//submit button function________________________________________
+	$("#submitbutton").click(function() {
+		//creating an array of all of the people counts for the HIT
+		img1Count = $("#numpeoplefield1").val();
+		$("#numpeoplefield").append(img1Count);
+		img2Count = $("#numpeoplefield2").val();
+		$("#numpeoplefield").append(img2Count);
+		img3Count = $("#numpeoplefield3").val();
+		$("#numpeoplefield").append(img3Count);
+		img4Count = $("#numpeoplefield4").val();
+		$("#numpeoplefield").append(img4Count);
+		img5Count = $("#numpeoplefield5").val();
+		$("#numpeoplefield").append(img5Count);
+		img6Count = $("#numpeoplefield6").val();
+		$("#numpeoplefield").append(img6Count);
+		//creating an array of all of the times for the HIT
+		img1Time= $("#timetaken1").val();
+		$("#timetaken").append(img1Time);
+		img2Time= $("#timetaken2").val();
+		$("#timetaken").append(img2Time);
+		img3Time= $("#timetaken3").val();
+		$("#timetaken").append(img3Time);
+		img4Time= $("#timetaken4").val();
+		$("#timetaken").append(img4Time);
+		img5Time= $("#timetaken5").val();
+		$("#timetaken").append(img5Time);
+		img6Time= $("#timetaken1").val();
+		$("#timetaken").append(img6Time);
 	});
 })
